15c036a50ae97bfd42105a34898442eddf31ec6b
JuniperTea <luvadergolder@yahoo.ca>
2023-03-27 13:47:57 -0700

    redux thunk lab

--- src/data/store.js
+++ src/data/store.js
@@ -1,13 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cartSlice";
 import catFactSlice from "./catFactSlice";
+import jokeSlice from "./jokeSlice";
 import songSlice from "./songSlice";
 
 export const store = configureStore({
     reducer: {
         cart: cartSlice.reducer,
         songs: songSlice.reducer,
-        catFact: catFactSlice.reducer
+        catFact: catFactSlice.reducer,
+        joke: jokeSlice.reducer
     }
 
 })
+
+
